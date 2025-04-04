5c71cc5d35ae93683eb38f06f6c0ba3263f07557
olivia <lwangaolivia20@gmail.com>
2025-04-04 20:08:01 +0300

    validtaxcalc

--- Taxcalc.py
+++ Taxcalc.py
@@ -1,11 +1,19 @@
 import unittest
 
+def taxcalc (earning: int):
+    if earning < 12000:
+        return "pay no tax"
+    elif 12000 < earning < 36000:
+        return (20/100)*earning 
+    else:
+        return (40/100)*earning
 
 class test_taxcalculation(unittest.TestCase):
     def test_valid_taxcalc(self):
-        self.assertequal(taxcalc(400), "pay no tax")
-        self.asserteqaual(taxcalc(20000),4000)
-        self.assertequal(taxcalc(50000),20000 )
+        self.assertEqual(taxcalc(800), "pay no tax")
+        self.assertEqual(taxcalc(20000), 4000)
+        self.assertEqual(taxcalc(50000), 20000)
+    
 
 if __name__=="__main__":
     unittest.main()
